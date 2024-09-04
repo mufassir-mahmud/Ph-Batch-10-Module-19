e2fc07249759f3d0cda58af1a9d48265f2421afe
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-04 12:06:11 +0600

    added breake

---
+++ break.js
@@ -0,0 +1,15 @@
+for(let i = 0; i<= 10; i++){
+    console.log(i);
+    if(i >= 5){
+        break;
+    }
+}
+
+let n = 55;
+while(n > 25){
+    console.log(n);
+    if(n < 50){
+        break;
+    }
+    n--;
+}
