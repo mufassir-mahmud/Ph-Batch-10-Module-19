02c16b4f61f64f1b732898a7b6a97c184f10d46d
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-04 12:26:45 +0600

    added continue

---
+++ continue.js
@@ -0,0 +1,15 @@
+// for(let i = 1; i <= 10; i++){
+//     if(i %2 !== 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let n= 0;
+while( n <= 40){
+    n++;
+    if(n % 5 !== 0){
+        continue
+    }
+    console.log(n);
+}
